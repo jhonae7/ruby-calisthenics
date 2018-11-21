f095e5fa2d2e678630ee2456507a21c1c10209ff
jhonae7 <jhonatan.espinoza15@unanleon.edu.ni>
2018-11-20 19:40:47 -0600

    dessert_spec.rb y dessert.rb terminado

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,6 +1,20 @@
 class Dessert
   def initialize(name, calories)
     # your code here
+    @name = name
+    @calories = calories
+  end
+  def name
+    @name
+  end
+  def name=(name)
+    @name = name
+  end
+  def calories
+    @calories
+  end
+  def calories=(calories)
+    @calories = calories
   end
   def healthy?
     # your code here
@@ -10,16 +24,34 @@ class Dessert
   end
   def delicious?
     # your code here
+    return true
   end
 end
 
 class JellyBean < Dessert
   def initialize(flavor)
     # your code here
+    @name = name
+    @calories = calories
+    @flavor = flavor
   end
   def delicious?
-    if @flavor == licorice
+    if @flavor == "licorice"
       return false
+    else
+      return true
     end
+  end
+  def name
+    @name ="vanilla jelly bean"
+  end
+  def calories
+    @calories=5
+  end
+  def flavor
+    @flavor
+  end
+  def flavor=(flavor)
+    @flavor = flavor
   end
 end
